39e13fb6ece764c2ab12145de358a208a58900f6
Luís Gama <luishngama@gmail.com>
2016-10-14 17:02:41 -0300

    Code update
    Items #3 and #5 were revised.

--- run_analysis.R
+++ run_analysis.R
@@ -8,8 +8,8 @@ unzip("./data/galaxySacc.zip", exdir = "./data")
 #Using readme.txt and features_info.txt to identify the purpose of each data set.
 
 #Read activity labels and features
-activityLabels <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
-features <- read.table("./data/UCI HAR Dataset/features.txt")
+activityLabels <- read.table("./data/UCI HAR Dataset/activity_labels.txt", colClasses =  c("character"))
+features <- read.table("./data/UCI HAR Dataset/features.txt", colClasses = c("character"))
 
 #Read train sets
 trainSet <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
@@ -26,18 +26,26 @@ trainSet <- cbind(trainSubjects, trainActivities, trainSet)
 testSet <- cbind(testSubjects, testActivities, testSet)
 mergedSet <- rbind(trainSet, testSet)
 
-#3 - Uses descriptive activity names to name the activities in the data set
 #4 - Appropriately labels the data set with descriptive variable names.
-featuresNames <- as.vector(features[,2])
-colnames(mergedSet) <- c("Subject", "Activity", featuresNames)
+colnames(mergedSet) <- c("subject", "activity", features[,2])
 
 #2 - Extracts only the measurements on the mean and standard deviation for each measure.
 library(data.table)
-subsetZero <- mergedSet[names(mergedSet) %in% c("Subject", "Activity")]
+subsetZero <- mergedSet[names(mergedSet) %in% c("subject", "activity")]
 subsetOne <- mergedSet[names(mergedSet) %like% "mean"]
 subsetTwo <- mergedSet[names(mergedSet) %like% "std"]
 mergedSet <- cbind(subsetZero, subsetOne, subsetTwo)
 
+#3 - Uses descriptive activity names to name the activities in the data set
+mergedSet$activity[mergedSet$activity == 1] <- tolower(activityLabels[1,2])
+mergedSet$activity[mergedSet$activity == 2] <- tolower(activityLabels[2,2])
+mergedSet$activity[mergedSet$activity == 3] <- tolower(activityLabels[3,2])
+mergedSet$activity[mergedSet$activity == 4] <- tolower(activityLabels[4,2])
+mergedSet$activity[mergedSet$activity == 5] <- tolower(activityLabels[5,2])
+mergedSet$activity[mergedSet$activity == 6] <- tolower(activityLabels[6,2])
+
 #5 - From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-tidyDS <- data.frame(sapply(mergedSet, mean))
+tidyDS <- melt(mergedSet, id = c("subject", "activity"))
+tidyDS <- dcast(tidyDS, subject + activity ~ variable, mean)
+
 write.table(tidyDS, "./data/tidyDS.txt", quote = F, row.names = F, col.names = F)
